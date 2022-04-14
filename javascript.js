aff5aee4d1cf0de5b652dd1f07645138674ddde6
Gregory Krieger <1111589@palmbeach.k12.fl.us>
2022-04-14 09:31:17 -0400

    if statement take 2

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ function computerPlay(){
     return randomHand;
 }
 
-console.log(computerPlay())
+//console.log(computerPlay())
 
 /*Write a function that plays a single round of Rock Paper Scissors. 
 The function should take two parameters - the playerSelection and computerSelection
@@ -24,19 +24,55 @@ The function should take two parameters - the playerSelection and computerSelect
  "You Lose! Paper beats Rock"*/ 
 
 
- let computerSelection = computerPlay();
+ //rocklet computerSelection = computerPlay();
  //let playerSelection = window.prompt("Please choose rock paper or scissors");
- let playerSelection = 'rock'
- 
+ //let playerSelection = 'rock'
+ //let computerSelection = 'rock';
+ /*if (computerSelection == 'rock' && playerSelection =='rock' || computerSelection == 'paper' && playerSelection == 'paper' || computerSelection == 'scissor' && playerSelection == 'scissor'){
+    result = 'tie';
+}
+else if(computerSelection == 'scissor' && playerSelection =='rock'){
+    result = 'computer loses'
+}
+else if(computerSelection == 'paper' && playerSelection =='rock'){
+result = 'computer wins'
+}
+else if(playerSelection == 'scissor' && computerSelection == 'rock'){
+result = 'player loses'
+}
+else if(){
+
+}
+else {
+result = 'you suck!'
+};*/
+const computerSelection = "rock"; 
+const playerSelection = "scissor";
+
+
 function round(computerSelection, playerSelection) {
     let result;
-    if (computerSelection == 'rock' && playerSelection =='rock');{
-            result = 'tie';
+    if (computerSelection === playerSelection){
+
+        result = 'tie';
     }
-    else if (computerSelection == 'rock' && playerSelection =='rock')
-    {   
-        result = 'computer wins';
+    else if(computerSelection != playerSelection){
+        if(computerSelection === "rock" && playerSelection === "scissor"){
+            result = "Computer wins"
+        }
+        else if(computerSelection === "rock" && playerSelection === "paper"){
+            result = "player wins"
+        }
+
     }
+    
     return result;
+    
 }
-console.log(round())
+
+//computerPlay();
+console.log(round(computerSelection, playerSelection));
+
+
+
+
