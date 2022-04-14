03ce1e5a10fd99cdedbe49722da1f2dd68442748
Greg <Gregory.Krieger94@gmail.com>
2022-04-14 00:20:01 -0400

    initial code for if statement for results

--- javascript.js
+++ javascript.js
@@ -17,3 +17,26 @@ function computerPlay(){
 }
 
 console.log(computerPlay())
+
+/*Write a function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - the playerSelection and computerSelection
+ - and then return a string that declares the winner of the round like so: 
+ "You Lose! Paper beats Rock"*/ 
+
+
+ let computerSelection = computerPlay();
+ //let playerSelection = window.prompt("Please choose rock paper or scissors");
+ let playerSelection = 'rock'
+ 
+function round(computerSelection, playerSelection) {
+    let result;
+    if (computerSelection == 'rock' && playerSelection =='rock');{
+            result = 'tie';
+    }
+    else if (computerSelection == 'rock' && playerSelection =='rock')
+    {   
+        result = 'computer wins';
+    }
+    return result;
+}
+console.log(round())
